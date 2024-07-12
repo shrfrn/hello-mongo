7303b328da432af4418fc3aed55b59f5af23c674
shrfrn <shrfrn@gmail.com>
2024-07-12 12:04:58 +0300

    Init repo

---
+++ server.js
@@ -0,0 +1,48 @@
+import { MongoClient } from 'mongodb'
+import { customerService } from './services/customer.service.js'
+
+// Connection URL
+const url = 'mongodb://localhost:27017'
+
+// Database Name
+const dbName = 'tester_db'
+
+helloMongo()
+// tryMongo()
+
+async function helloMongo() {
+
+    var customers = await customerService.query()
+    console.log('Got Customers: ', customers)
+
+    // var customer = await customerService.getById('66460a50e392535142549cd3')
+    // console.log('Got Customer: ', customer)
+
+    // customer.balance = 100
+    // var updatedCustomer = await customerService.update(customer)
+    // console.log('Updated Customer: ', updatedCustomer)
+
+    // const newCustomer = { fullName: 'Shumuts', balance: 220 }
+
+    // var addedCustomer = await customerService.add(newCustomer)
+    // console.log('Added Customer: ', addedCustomer)
+
+    // const { deletedCount } = await customerService.remove('6644ec4d1f6e9d64689bfe4a')
+    // console.log('Customer Removed?', deletedCount)
+}
+
+async function tryMongo() {
+
+    console.log('Connecting')
+    const connection = await MongoClient.connect(url)
+    
+    console.log('Connected. Getting DB')
+    const db = connection.db(dbName)
+
+    console.log('Getting Collection')
+    const collection = db.collection('customer')
+
+    console.log('Fetching Docs.')
+    const docs = await collection.find({ balance: { $gte: 10 } }).limit(3).toArray()    // connection.close()
+    console.log(`Docs:\n`, docs)
+}
---
+++ services/customer.service.js
@@ -0,0 +1,77 @@
+import { ObjectId } from 'mongodb'
+import { dbService } from './db.service.js'
+
+export const customerService = {
+    query,
+    getById,
+    remove,
+    update,
+    add
+}
+
+async function query(filterBy = { txt: 's' }) {
+    const criteria = {}
+    if (filterBy.txt) {
+        const regex = new RegExp(filterBy.txt, 'i')
+        criteria.fullName = { $regex: regex }
+    }
+    if (filterBy.minBalance) {
+        criteria.balance = { $gte: filterBy.minBalance }
+    }
+
+    console.log('criteria', criteria)
+
+    try {
+        const collection = await dbService.getCollection('customer')
+        const customers = await collection.find(criteria).toArray()
+        return customers
+    } catch (err) {
+        console.log('ERROR: cannot find customers')
+        throw err
+    }
+}
+
+async function getById(customerId) {
+    try {
+        const collection = await dbService.getCollection('customer')
+        const customer = await collection.findOne({ _id: ObjectId.createFromHexString(customerId) })
+        return customer
+    } catch (err) {
+        console.log(`ERROR: cannot find customer ${customerId}`)
+        throw err
+    }
+}
+
+async function remove(customerId) {
+    try {
+        const collection = await dbService.getCollection('customer')
+        return await collection.deleteOne({ _id: ObjectId.createFromHexString(customerId) })
+    } catch (err) {
+        console.log(`ERROR: cannot remove customer ${customerId}`)
+        throw err
+    }
+}
+
+async function update(customer) {
+    try {
+        const collection = await dbService.getCollection('customer')
+        await collection.updateOne({ _id: customer._id }, { $set: customer })
+        return customer
+    } catch (err) {
+        console.log(`ERROR: cannot update customer ${customer._id}`)
+        throw err
+    }
+}
+
+async function add(customer) {
+    try {
+        const collection = await dbService.getCollection('customer')
+        await collection.insertOne(customer)
+        console.log('customer', customer)
+
+        return customer
+    } catch (err) {
+        console.log(`ERROR: cannot insert customer`)
+        throw err
+    }
+}
